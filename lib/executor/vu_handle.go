cabd96ecce6c0bb9addfe4c231b16a0c54271cf0
Ivan Mirić <ivan@loadimpact.com>
2021-02-15 16:42:59 +0100

    Add getScenarioStats to k6/execution
    Part of #1320

--- lib/executor/vu_handle.go
+++ lib/executor/vu_handle.go
@@ -88,11 +88,12 @@ short names for input:
 // - it's not required but preferable, if where possible to not reactivate VUs and to reuse context
 // as this speed ups the execution
 type vuHandle struct {
-	mutex     *sync.Mutex
-	parentCtx context.Context
-	getVU     func() (lib.InitializedVU, error)
-	returnVU  func(lib.InitializedVU)
-	config    *BaseConfig
+	mutex         *sync.Mutex
+	parentCtx     context.Context
+	getVU         func() (lib.InitializedVU, error)
+	returnVU      func(lib.InitializedVU)
+	config        *BaseConfig
+	scenarioState *lib.ScenarioState
 
 	initVU       lib.InitializedVU
 	activeVU     lib.ActiveVU
